db50c9ca168cfa124d131188c57abd6e7f6599d4
yanagiharah <yanagiharah@analix.co.jp>
2024-07-12 15:25:19 +0900

    MustUserの追加。

---
+++ src/main/java/com/example/demo/model/MustUser.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class MustUser {
+	
+	private int id;
+	
+	private String name;
+	
+	private String password;
+	
+	private String role;
+	
+	private Date startDate;
+	
+	
+	
+
+}
