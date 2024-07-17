b2f6d2f7c6c2a0e09122e8c2b2e9ecde046d9d84
k-sumiki <sumikik@analix.co.jp>
2024-07-17 16:04:40 +0900

    komo

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -22,34 +22,18 @@ public class LoginController {
 		}
 		
 		@RequestMapping("/check")
-			public String check(Integer userId ,String password,Model model, RedirectAttributes redirectAttributes ){
-			
-//			try {
-//				if(userId ==null|| password == '\u0000' ) {
-//					redirectAttributes.addFlashAttribute("out","登録されているデータと一致しません。");
-//					return "redirect:/";
-//				}
-//				
-//			}catch(methodArgumentTypeMismatchException e) {
-//				redirectAttributes.addFlashAttribute("out","登録されているデータと一致しません。");
-//				return "redirect:/";
-//			}
-			
-			
-			
-			//ユーザーID,パスワードを引数にserviceクラスでリストの取得
+		public String check(Integer userId, String password, Model model, RedirectAttributes redirectAttributes) {
+
 			Users users = loginService.LoginListUp(userId, password);
-			System.out.print(users);
-			//リストがあった場合
-			if(users == null) {
-				redirectAttributes.addFlashAttribute("out","登録されているデータと一致しません。");
+
+			if (users == null) {
+				redirectAttributes.addFlashAttribute("out", "登録されているデータと一致しません。");
 				return "redirect:/";
-			}
-			else if(users.getRole() .equalsIgnoreCase("Admin")) {
+			} else if (users.getRole().equalsIgnoreCase("Admin")) {
 				model.addAttribute("Users", users);
 				return "User/manegement";
 			}
 			model.addAttribute("Users", users);
 			return "attendance/registration";
 		}
-}
+	}
