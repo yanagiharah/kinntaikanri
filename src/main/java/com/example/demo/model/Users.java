a5afeb7784fca85f933a9373579b8c7de2a7ba38
k-sumiki <sumikik@analix.co.jp>
2024-07-19 15:33:23 +0900

    金曜15:30

--- src/main/java/com/example/demo/model/Users.java
+++ src/main/java/com/example/demo/model/Users.java
@@ -3,8 +3,6 @@ package com.example.demo.model;
 import java.io.Serializable;
 import java.util.Date;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
@@ -12,7 +10,6 @@ import lombok.Data;
 @Component
 @SessionScope
 @Data
-@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Users implements Serializable {
 	
 	private Integer userId;
