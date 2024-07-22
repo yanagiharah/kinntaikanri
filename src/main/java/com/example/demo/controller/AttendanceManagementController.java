0d1ede0c3d2077d398eabba2b2c2832608892d1f
k-sumiki <sumikik@analix.co.jp>
2024-07-22 13:31:51 +0900

    勤怠

--- src/main/java/com/example/demo/controller/AttendanceManagementController.java
+++ src/main/java/com/example/demo/controller/AttendanceManagementController.java
@@ -2,8 +2,6 @@ package com.example.demo.controller;
  
 import java.util.List;
 
-import jakarta.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +11,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.example.demo.model.Attendance;
 import com.example.demo.model.Users;
 import com.example.demo.service.AttendanceManagementService;
+
+import jakarta.servlet.http.HttpSession;
  
 @Controller
 @RequestMapping("/attendance")
@@ -63,8 +63,11 @@ public class AttendanceManagementController {
 	
 	
 	@RequestMapping("/registration")
-	public String insert(List<Attendance> attendance) {
-		System.out.print(attendance);
+	public String insert(String startTime,String endTime,Model model,HttpSession session) {
+		Users users = (Users) session.getAttribute("Users");
+		model.addAttribute("Users", users);
+		System.out.print(startTime);
+		System.out.println(endTime);
 		return "attendance/registration";
 		
 	}
