78808d2b2eab893ac708906b9adc0607801bac3e
k-sumiki <sumikik@analix.co.jp>
2024-07-22 16:40:57 +0900

    調べる

--- src/main/java/com/example/demo/controller/AttendanceManagementController.java
+++ src/main/java/com/example/demo/controller/AttendanceManagementController.java
@@ -1,14 +1,17 @@
 package com.example.demo.controller;
  
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.model.Attendance;
+import com.example.demo.model.AttendanceFormList;
 import com.example.demo.model.Users;
 import com.example.demo.service.AttendanceManagementService;
 
@@ -20,6 +23,8 @@ public class AttendanceManagementController {
 	@Autowired
 	private AttendanceManagementService attendanceManagementService;
 	
+	
+	
 	@RequestMapping("/index")
 		public String start(HttpSession session, Model model){
 		Users users = (Users) session.getAttribute("Users");
@@ -28,46 +33,40 @@ public class AttendanceManagementController {
 	}
 	
 	
-	@RequestMapping("/management")
-	public String attendanceSearch(Integer userId, Integer years, Integer month, Model model, RedirectAttributes redirectAttributes, HttpSession session){
-//		System.out.print("『画面から受け取りチェック表示』："+ userId + years + month);
-//		System.out.print("『ユーザーID』：" + userId);
-//	if(name == null) {
-//		redirectAttributes.addFlashAttribute("check","入力は必須です。");
-//		return "redirect:/User/manegement";
-//	}
-	//名前を引数にserviceクラスでリストの取得
-//		System.out.print("ここに表示"+userName);
+	@RequestMapping(value = "/management", params = "search", method = RequestMethod.POST)
+	public String attendanceSearch(Integer userId, Integer years, Integer month, Model model,
+			RedirectAttributes redirectAttributes, HttpSession session) {
+
 		Users users = (Users) session.getAttribute("Users");
 		model.addAttribute("Users", users);
-		
-		
 
-			if (years == null || month == null)  {
-				model.addAttribute("check", "年月を入力してください");
-				return "attendance/registration";
-			}
-			
-		
-		
+		if (years == null || month == null) {
+			model.addAttribute("check", "年月を入力してください");
+			return "attendance/registration";
+		}
 		
 		List<Attendance> attendance = attendanceManagementService.attendanceSearchListUp(userId, years, month);
-	if(attendance != null) {
-		model.addAttribute("Attendance", attendance);
-		
+		if (attendance != null) {
+			//formに詰めなおす
+			AttendanceFormList attendanceFormList = new AttendanceFormList();
+			ArrayList<Attendance> attendanceList = new  ArrayList<Attendance>();
+			attendanceFormList.setAttendanceList(attendanceList);
+			attendanceList.addAll(attendance);
+			model.addAttribute("attendanceFormList", attendanceFormList);
+
+			return "attendance/registration";
+		}
+
 		return "attendance/registration";
 	}
 	
-	return "attendance/registration";
-	}
 	
-	
-	@RequestMapping("/registration")
-	public String insert(String startTime,String endTime,Model model,HttpSession session) {
+	@RequestMapping(value = "/management", params ="insert" ,method = RequestMethod.POST )
+	public String insert(AttendanceFormList attendanceFormList,Model model,HttpSession session) {
 		Users users = (Users) session.getAttribute("Users");
 		model.addAttribute("Users", users);
-		System.out.print(startTime);
-		System.out.println(endTime);
+		System.out.print("登録後"+attendanceFormList.getAttendanceList());
+		//System.out.print("登録後"+status);
 		return "attendance/registration";
 		
 	}
---
+++ src/main/java/com/example/demo/model/AttendanceFormList.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import java.util.ArrayList;
+
+import lombok.Data;
+
+@Data
+public class AttendanceFormList {
+	
+	private ArrayList<Attendance> attendanceList;
+}
