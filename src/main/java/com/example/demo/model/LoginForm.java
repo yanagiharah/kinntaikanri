d5c6694625f98f1f37a35b19073864a5078e1fe6
k-sumiki <sumikik@analix.co.jp>
2024-08-01 15:16:00 +0900

    ログインform

--- src/main/java/com/example/demo/model/LoginForm.java
+++ src/main/java/com/example/demo/model/LoginForm.java
@@ -1,10 +1,8 @@
 package com.example.demo.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -12,11 +10,11 @@ public class LoginForm {
 	
 	@NotBlank(message="ユーザーIDは必須です")
 	@Length(max = 16)
-	@Pattern(regexp = "^[0-9]+$")
+	//@Pattern(regexp = "^[0-9]+$")
 	public String userId;
 	
 	@NotBlank(message="パスワードは必須です")
 	@Length(max = 16)
-	@Pattern(regexp = "^[a-zA-Z0-9]+$")
+	//@Pattern(regexp = "^[a-zA-Z0-9]+$")
 	public String password; 
 }
