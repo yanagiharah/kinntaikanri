4275ec342056029d43a8fd9458d38a640479a419
k-sumiki <sumikik@analix.co.jp>
2024-08-06 11:30:10 +0900

    修正

--- src/main/java/com/example/demo/mapper/UsersMapper.java
+++ src/main/java/com/example/demo/mapper/UsersMapper.java
@@ -1,27 +1,14 @@
 package com.example.demo.mapper;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.example.demo.model.Users;
 
 @Mapper
-public interface UserMapper {
-	
-	//全件取得
-	List<Users> selectAll();
+public interface UsersMapper {
 	
 	//１件取得
 	Users selectByPrimaryKey(@Param("userId")Integer userId, @Param("password")String password);
 	
-	//登録
-	int insert(Users user);
-	
-	//更新
-	int updateByPrimaryKey(Users user);
-	
-	//削除
-	int deleteByPrimaryKey(Long id);
 }
--- src/main/java/com/example/demo/service/LoginService.java
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -3,15 +3,15 @@ package com.example.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.mapper.UserMapper;
+import com.example.demo.mapper.UsersMapper;
 import com.example.demo.model.Users;
 
 @Service
 public class LoginService {
   @Autowired
-  private UserMapper userMapper;
+  private UsersMapper usersMapper;
 	public Users LoginListUp(Integer userId ,String password) {
-		Users users = userMapper.selectByPrimaryKey(userId, password);
+		Users users = usersMapper.selectByPrimaryKey(userId, password);
 		return users;
 		
 	}
