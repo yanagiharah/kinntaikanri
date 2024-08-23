84e32b59b6eb1a4fead2076af43ad8cc02ce1d30
k-sumiki <sumikik@analix.co.jp>
2024-08-23 14:07:32 +0900

    javaScript

--- src/main/resources/static/js/Report.js
+++ src/main/resources/static/js/Report.js
@@ -35,3 +35,37 @@ $(document).ready(function() {
     });
     
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // テーブル内の「作業時間」と「作業内容」のすべての入力フィールドを取得
+    const dailyReportDetailTime = document.querySelectorAll('input[name^="dailyReportDetailForm"][name$=".dailyReportDetailTime"]');
+    const content = document.querySelectorAll('input[name^="dailyReportDetailForm"][name$=".content"]');
+    //const 提出ボタンを取得
+   // const submissionButton =document.getElementById("submission");
+    
+    // 作業時間の入力フィールドに対してinputイベントを設定
+    dailyReportDetailTime.forEach(input => {
+        input.addEventListener('input', function(event) {
+            if (event.target.value.trim() == "") {
+				//提出ボタンを活性化
+                console.log("作業時間が入力されました: " + event.target.value);
+                document.getElementById("submission").disabled = true;
+            } else {
+				document.getElementById("submission").disabled = false;
+                console.log("作業時間の入力がクリアされました");
+            }
+        });
+    });
+
+    // 作業内容の入力フィールドに対してinputイベントを設定
+    content.forEach(input => {
+        input.addEventListener('input', function(event) {
+            if (event.target.value.trim() !== "") {
+				//提出ボタンを活性化
+                console.log("作業内容が入力されました: " + event.target.value);
+            } else {
+                console.log("作業内容の入力がクリアされました");
+            }
+        });
+    });
+});
