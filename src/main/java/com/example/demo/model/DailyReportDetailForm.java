9a0a53ebc9f5b590ae87bdba7bf3684a003c2f43
k-sumiki <sumikik@analix.co.jp>
2024-09-04 11:31:20 +0900

    メッセージ関係(勤怠)

--- src/main/java/com/example/demo/model/DailyReportDetailForm.java
+++ src/main/java/com/example/demo/model/DailyReportDetailForm.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
-
 import lombok.Data;
 
 @Data
