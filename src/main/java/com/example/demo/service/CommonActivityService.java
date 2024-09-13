03e264caca6acdc4413d15ca8114aa4cda968eef
yanagiharah <yanagiharah@analix.co.jp>
2024-09-13 17:57:52 +0900

    先月の月次勤怠申請がマネージャーに却下されていた場合、ログイン時に画面に表示される機能実装。

--- src/main/java/com/example/demo/service/CommonActivityService.java
+++ src/main/java/com/example/demo/service/CommonActivityService.java
@@ -1,14 +1,18 @@
 package com.example.demo.service;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import jakarta.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import com.example.demo.inter.MessageOutput;
+import com.example.demo.mapper.MonthlyAttendanceReqMapper;
+import com.example.demo.model.MonthlyAttendanceReq;
 import com.example.demo.model.Users;
-
-import jakarta.servlet.http.HttpSession;
 
 @Service
 public class CommonActivityService {
@@ -20,10 +24,13 @@ public class CommonActivityService {
 	
 	private final MessageOutput messageOutput;
 	
-	CommonActivityService(DailyReportService dailyReportService,AttendanceManagementService attendanceManagementService,MessageOutput messageOutput){
+	private final MonthlyAttendanceReqMapper monthlyAttendanceReqMapper;
+	
+	CommonActivityService(DailyReportService dailyReportService,AttendanceManagementService attendanceManagementService,MessageOutput messageOutput, MonthlyAttendanceReqMapper monthlyAttendanceReqMapper){
 		this.dailyReportService = dailyReportService;
 		this.attendanceManagementService = attendanceManagementService;
 		this.messageOutput = messageOutput;
+		this.monthlyAttendanceReqMapper = monthlyAttendanceReqMapper;
 	}
 
 
@@ -38,7 +45,7 @@ public class CommonActivityService {
 	public Model backMenu(Model model, HttpSession session) {
 		 usersModelSession(model,session);
 		 Users users = (Users) model.getAttribute("Users");
-		model.addAttribute("Users", users);
+		 model.addAttribute("Users", users);
 		if ("Regular".equals(users.getRole()) || "UnitManager".equals(users.getRole())) {
 			LocalDate today = LocalDate.now();
 			LocalDate yesterday = today.minusDays(1);
@@ -50,6 +57,19 @@ public class CommonActivityService {
 			}
 			if (checkAttendance == 0) {
 				model.addAttribute("CheckAttendance", messageOutput.message("checkAttendance"));
+			}
+			
+			// 現在の日付を取得
+			LocalDate now = LocalDate.now();
+			// 先月の1日を取得
+			LocalDate firstDayOfLastMonth = now.minusMonths(1).withDayOfMonth(1);
+			// Date型に変換
+			Date firstDayOfLastMonthDate = Date.from(firstDayOfLastMonth.atStartOfDay(ZoneId.systemDefault()).toInstant());
+			//先月のmonthlyAttendanceReqを変数に詰める
+			MonthlyAttendanceReq monthlyAttendanceReq = monthlyAttendanceReqMapper.selectTargetYearMonthStatus(firstDayOfLastMonthDate, users.getUserId());
+			//先月のmonthlyAttendanceReqが存在し、かつ月次勤怠承認状況をあらわすstatusが３（却下）のとき、処理メニュー画面にメッセージを表示させる。
+			if(monthlyAttendanceReq != null && monthlyAttendanceReq.getStatus() == 3) {
+				model.addAttribute("monthlyAttendanceStatusIsThree",messageOutput.message("monthlyAttendanceStatusIsThree"));
 			}
 		}
 		return model;
