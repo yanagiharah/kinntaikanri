e5fdf1076491314040aa467a980797b6269c2283
SatoK330 <satok@analix.co.jp>
2024-12-04 17:56:45 +0900

    js追加

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -6,4 +6,14 @@
         history.pushState(null, null, location.href);
     };
 })();
-
+//ボタン連打防止機能　懸念点常に二重送信になっているのでは？という可能性はある。可能性だけ
+function disableSubmit(button){
+	//ボタンを無効化
+	 button.disabled = true;
+	 //ボタンに紐づいたform送信
+	 button.form.submit();
+	 //5秒後にボタン有効化・5秒はパスワードリセットのメール送信ボタンにマージンを取った値
+	    setTimeout(function() {
+	        button.disabled = false;
+	    }, 5000);
+	}
