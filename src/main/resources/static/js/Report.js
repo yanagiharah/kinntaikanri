9a98254eee18c4cd003196ebe9cd61561849bfbf
SatoK330 <satok@analix.co.jp>
2024-12-06 16:14:45 +0900

    日報管理一般側のボタン活性条件を追加

--- src/main/resources/static/js/Report.js
+++ src/main/resources/static/js/Report.js
@@ -1,3 +1,7 @@
+// テーブル内の「作業時間」と「作業内容」のすべての入力フィールドを取得
+const dailyReportDetailTime = Array.from(document.querySelectorAll('input[name^="dailyReportDetailForm"][name$=".dailyReportDetailTime"]'));
+const content = Array.from(document.querySelectorAll('input[name^="dailyReportDetailForm"][name$=".content"]'));
+
 $(document).ready(function() {
     $('#today').change(function() {
         // フォームを自動的に送信
@@ -19,93 +23,68 @@ function setTodayDate() {
     }
 }
 
-//function sendData() {
-//    // body内のデータを取得
-//    const dailyReportDate = document.querySelector('input[name="dailyReportDate"]').value;
-//    const dailyReportId = document.querySelector('input[name="dailyReportId"]').value;
-//    const userId = document.querySelector('input[name="userId"]').value;
-//    const status = document.querySelector('input[name="status"]').value;
-//
-//    // データを送信する処理
-//    fetch('/daily/detailUpdate', {
-//        method: 'POST',
-//        headers: {
-//            'Content-Type': 'application/json'
-//        },
-//        body: JSON.stringify({
-//            dailyReportDate: dailyReportDate,
-//            dailyReportId: dailyReportId,
-//            userId: userId,
-//            status: status
-//        })
-//    })
-//    .then(response => {
-//        if (!response.ok) {
-//            throw new Error('Network response was not ok');
-//        }
-//        return response.json();
-//    })
-//    .then(data => {
-//        console.log('Success:', data);
-//        // 必要に応じて、成功時の処理をここに追加
-//    })
-//    .catch(error => {
-//        console.error('Error:', error);
-//        // エラーハンドリングをここに追加
-//    });
-//}
+function checkTable() {
+	// テーブル内の「作業時間」と「作業内容」のすべての入力フィールドを取得
+	const dailyReportDetailId = Array.from(document.querySelectorAll('input[name^="dailyReportDetailForm"][name$=".dailyReportDetailId"]'));
+	const status =  Array.from(document.querySelectorAll('input[name^="status"]'));
+	let activeFlag = false;//ボタンフラグ
+	let changedData ;//変更有フラグ
+	
+	//承認済みを定義
+	const isStatusInvalid = status[0].value == 2;
+	
+	//承認済みならスキップ
+	if(!isStatusInvalid){
+		
+	// 入力フィールドを全てチェック
+	dailyReportDetailTime.some((taskTime, index) => {
+
+//		//taskTime 作業時間
+		const taskContent = content[index]; //作業内容
+		const detailId = dailyReportDetailId[index]; //日報(サブ)ID
+		
+		//日報IDをもっていて変更がない時
+		if (dailyReportDetailId && !changedData) {
+				//taskTimeを数字型に変更（比べる対象が数字型であるため）
+				  let taskTimeInt = parseInt(taskTime.value.trim(), 10);
+			  //作業内容作業時間をともに元データと比べて一致していた場合
+				  if (taskContent.value.trim() === dailyReportDetailForm[index].content && taskTimeInt === dailyReportDetailForm[index].dailyReportDetailTime) {
+				 	activeFlag = false;
+			//またはボタンが非活性で作業内容・時間共に入力済みならば
+			  } else if (!activeFlag && taskTime.value.trim() !== "" && taskContent.value.trim() !== "") {
+		      activeFlag = true;
+			  changedData = true;//変更有フラグをオンに
+			  }
+		//日報IDをもっておらず、ボタンが非活性かつ、作業時間内容共に入力済みならば
+		} else if (!activeFlag && taskTime.value.trim() !== "" && taskContent.value.trim() !== "") {
+		  activeFlag = true;
+		}
+	
+		//こちらは常に判定（強制無効化処理なので）
+		//作業時間と作業内容が共に空欄ではなく、片方のみ空欄の場合
+		if((taskTime.value.trim() ==="" || taskContent.value.trim() ==="") && !(taskContent.value.trim() === "" && taskTime.value.trim() === "")){
+			activeFlag = false;
+			return true;//コールバック関数（dailyReportDetailTime.some((taskTime, index)）を強制終了
+		}
+			
+			
+		})
+	}
+
+	//承認済みなら
+    if (isStatusInvalid) {
+        activeFlag = false;
+    }
+		
+	// 結果に応じて提出ボタンを活性化/非活性化　activeFlag=true=> 有効化　activeFlag=false =>無効
+	document.getElementById("submission").disabled = !activeFlag;
+	
+}
+
 
 
  if(userRole != "Manager"){
 document.addEventListener('DOMContentLoaded', function() {
-	// テーブル内の「作業時間」と「作業内容」のすべての入力フィールドを取得
-	const dailyReportDetailTime = Array.from(document.querySelectorAll('input[name^="dailyReportDetailForm"][name$=".dailyReportDetailTime"]'));
-	const content = Array.from(document.querySelectorAll('input[name^="dailyReportDetailForm"][name$=".content"]'));
-	const dailyReportDetailId = Array.from(document.querySelectorAll('input[name^="dailyReportDetailForm"][name$=".dailyReportDetailId"]'));
-	const status =  Array.from(document.querySelectorAll('input[name^="stat"][name$="us"]'));
-	let activeFlag = false;
-    
-	function checkTable() {
- 
-		// 入力フィールドを全てチェック
-		dailyReportDetailTime.some((taskTime, index) => {
- 
-			//taskTime 作業時間
-			const taskContent = content[index]; //作業内容
-			const detailId = dailyReportDetailId[index]; //日報(サブ)ID
- 
-			//日報(サブ).IDがある場合： 作業時間または作業内容が空白、かつIDが存在する場合、活性にする
-			if (detailId.value.trim() !== ""&& taskContent.value.trim() !== "") {
-				if (taskTime.value.trim() == "" || taskContent.value.trim() == "") {	
-					if( (taskTime.value.trim() == "" && taskContent.value.trim() === "")){
-						activeFlag = true
-					}else{
-						activeFlag = false;
-					}
-				} else {
-					activeFlag = true;
-				}
-			} else { //日報(サブ).IDがない場合
-				if ((taskTime.value.trim() != "" && taskContent.value.trim() != "")) {
-					activeFlag = true;
-				}
-				else if ((taskTime.value.trim() != "" || taskContent.value.trim() != "")) {
-					activeFlag = false;
-				}
-			}
-		});
-		if (status.length > 0) {
-		    const isStatusInvalid = status[0].value == 2;
-
-		    if (isStatusInvalid) {
-		        activeFlag = false;
-		    }
-
-		    // 結果に応じて提出ボタンを活性化/非活性化
-		    document.getElementById("submission").disabled = !activeFlag;
-		}
-	}
- 
     // 全ての作業時間と作業内容の入力フィールドに対してinputイベントを設定
     dailyReportDetailTime.forEach(input => input.addEventListener('input', checkTable));
     content.forEach(input => input.addEventListener('input', checkTable));
@@ -161,7 +140,42 @@ document.addEventListener('DOMContentLoaded', function() {
 //});
  
 // ----------------------------------------------------------下記使用していない(★とっておきたい)----------------------------------------
- 
+//function sendData() {
+//    // body内のデータを取得
+//    const dailyReportDate = document.querySelector('input[name="dailyReportDate"]').value;
+//    const dailyReportId = document.querySelector('input[name="dailyReportId"]').value;
+//    const userId = document.querySelector('input[name="userId"]').value;
+//    const status = document.querySelector('input[name="status"]').value;
+//
+//    // データを送信する処理
+//    fetch('/daily/detailUpdate', {
+//        method: 'POST',
+//        headers: {
+//            'Content-Type': 'application/json'
+//        },
+//        body: JSON.stringify({
+//            dailyReportDate: dailyReportDate,
+//            dailyReportId: dailyReportId,
+//            userId: userId,
+//            status: status
+//        })
+//    })
+//    .then(response => {
+//        if (!response.ok) {
+//            throw new Error('Network response was not ok');
+//        }
+//        return response.json();
+//    })
+//    .then(data => {
+//        console.log('Success:', data);
+//        // 必要に応じて、成功時の処理をここに追加
+//    })
+//    .catch(error => {
+//        console.error('Error:', error);
+//        // エラーハンドリングをここに追加
+//    });
+//} 
+
 // 選択した日付をサーバーに送信する関数(Ajax)
 //function sendSelectedDate() {
 //    var selectedDate = $('#today').val();
